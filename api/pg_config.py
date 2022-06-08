a2590ba5d839f3aed9c2e5d04d60cd209aa69647
Xander" Xin Chen <xander.chen@blackwoodseven.com>
2022-06-08 16:28:44 -0700

    Update pg_config.py

--- api/pg_config.py
+++ api/pg_config.py
@@ -1,7 +1,7 @@
 class dbsettings:
     user = "postgres" 
-    password = "linjie1018"
-    host = "192.168.1.187"
+    password = "*****"
+    host = "******"
     port = 5432
     database = "postgres"
     pg_url = f'postgresql://{user}:{password}@{host}:{port}/{database}'
